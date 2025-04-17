5ec69412ba18bef745fed0a85810b91df3fc6e3a
Hamlet Abbaszade <hamletabbaszade0@gmail.com>
2025-04-17 20:51:59 +0400

    updated

--- src/main/java/www/library/net/controller/ReaderController.java
+++ src/main/java/www/library/net/controller/ReaderController.java
@@ -1,9 +1,19 @@
 package www.library.net.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import www.library.net.entity.Reader;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/reader")
 public class ReaderController {
+
+    @GetMapping
+    public List<Reader> getReaders(){/// murad and Hamlet
+
+        return null;
+    }
 }
